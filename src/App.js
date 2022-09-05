d9843bb2158e1b12b7ebbe4ecdac3fcb2977b906
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-09-05 22:27:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Activities from './pages/Activities/Activities';
+import Accomodation from './pages/Accomodation/Accomodation';
+import Deals from './pages/Deals/Deals';
+import Contact from './pages/Contact/Contact';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/activities' element={<Activities/>}/>
+        <Route path='/accomodation' element={<Accomodation/>}/>
+        <Route path='/deals' element={<Deals/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Accomodation/Accomodation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Accomodation() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Accomodation
